18ee4c4ec56aa37a1f07831b2c1e87a68acdce80
Alexander Santos <alexsantosantana@live.com>
2023-01-12 19:24:59 -0500

    Update version to 0.0.6

--- minyan-times.php
+++ minyan-times.php
@@ -5,7 +5,7 @@
 /*
   Plugin Name: Minyan Times
   Description: A component that organizes prayer times by location or time block...
-  Version: 0.0.5
+  Version: 0.0.6
   Author: Alexander Santos
 */
 
